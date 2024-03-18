6bd2c8d4f980710cbff1fd14b665c8945695386c
Anindita H.K <155972860+A-HK-KD@users.noreply.github.com>
2024-03-18 21:28:47 +0530

    cleaned up commented-out code

--- src/redux/slices/languageSlice.ts
+++ src/redux/slices/languageSlice.ts
@@ -1,27 +1,3 @@
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-// interface LanguageState {
-//   selectedLanguage: string;
-// }
-
-// const initialState: LanguageState = {
-//   selectedLanguage: 'en',
-// };
-
-// const languageSlice = createSlice({
-//   name: 'language',
-//   initialState,
-//   reducers: {
-//     changeLanguage(state, action: PayloadAction<string>) {
-//       state.selectedLanguage = action.payload;
-//     },
-//   },
-// });
-
-// export const { changeLanguage } = languageSlice.actions;
-
-// export default languageSlice.reducer;
-
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface LanguageState {
