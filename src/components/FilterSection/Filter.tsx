7a536f981641b02bf390730f9ef709430b95bea2
baneet24 <baneet.singh@kickdrumtech.com>
2024-03-25 11:43:26 +0530

    applying filters

--- src/components/FilterSection/Filter.tsx
+++ src/components/FilterSection/Filter.tsx
@@ -1,62 +1,174 @@
+// import { Box, Checkbox, ListItemIcon, ListItemText, MenuItem, Typography } from "@mui/material";
+// import Accordion from "@mui/material/Accordion";
+// import AccordionSummary from "@mui/material/AccordionSummary";
+// import AccordionDetails from "@mui/material/AccordionDetails";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import React, { useState } from "react";
+// import { OptionText, StyledBox } from "./FilterStyled";
+// import {
+//   StyledAccordion,
+//   StyledAccordionSummary,
+//   StyledAccordionDetails,
+//   StyledTypography,
+//   StyledMenuItem,
+//   StyledCheckbox,
+// } from "./FilterStyled";
+
+// const Filter = () => {
+
+
+//   const filters = [
+//     {
+//       filterName: 'Bed Type',
+//       show: true,
+//       options: ['King', 'Queen'],
+//       selectedOptions: [], 
+//     },
+//     {
+//       filterName: 'Room Type',
+//       show: true,
+//       options: ['Deluxe', 'Suite'],
+//       selectedOptions: [], 
+//     },
+//     {
+//       filterName: 'Price',
+//       show: true,
+//       options: ['<$100', '<$200'],
+//       selectedOptions: [],
+//     },
+//   ];
+  
+
+//   return (
+//     <StyledBox>
+//       <StyledAccordion>
+//         <StyledAccordionSummary
+//           expandIcon={<ExpandMoreIcon />}
+//         >
+//         <StyledTypography>
+//           {"Narrow Your Results"}
+//         </StyledTypography>                
+//         </StyledAccordionSummary>
+//         <StyledAccordionDetails>
+//           <div>
+//             {filters.map((filter) => {
+//               return (
+//                 filter.show && (
+//                   <StyledAccordion
+//                     key={filter.filterName}
+//                   >
+//                     <StyledAccordionSummary
+//                       expandIcon={<ExpandMoreIcon />}
+//                       aria-controls="panel1a-content"
+//                       id="panel1a-header"
+//                     >
+//                       <Typography>{filter.filterName}</Typography>
+//                     </StyledAccordionSummary>
+//                     <StyledAccordionDetails>
+//                       {filter.options.map((option) => {
+//                         return (
+//                           <MenuItem key={option}>
+//                             <ListItemIcon>
+//                               <Checkbox
+//                                 disabled={false}
+//                                 checked={filter.selectedOptions?.includes(option)}
+//                               />
+//                               <OptionText>{option}</OptionText>
+//                             </ListItemIcon>
+//                           </MenuItem>
+//                         );
+//                       })}
+//                     </StyledAccordionDetails>
+//                   </StyledAccordion>
+//                 )
+//               );
+//             })}
+//           </div>
+//         </StyledAccordionDetails>
+//       </StyledAccordion>
+//     </StyledBox>
+//   );
+// };
+
+// export default Filter;
+
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Box, Checkbox, ListItemIcon, ListItemText, MenuItem, Typography } from "@mui/material";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import React, { useState } from "react";
 import { OptionText, StyledBox } from "./FilterStyled";
-import {
-  StyledAccordion,
-  StyledAccordionSummary,
-  StyledAccordionDetails,
-  StyledTypography,
-  StyledMenuItem,
-  StyledCheckbox,
-} from "./FilterStyled";
+import { StyledAccordion, StyledAccordionSummary, StyledAccordionDetails, StyledTypography } from "./FilterStyled";
+import { setMinPrice, setMaxPrice, setBeds, setRoomTypes, setBedType } from "../../../src/redux/slices/roomsSlice"
 
-const Filter = () => {
+const Filter: React.FC = () => {
+  const dispatch = useDispatch();
 
+  const handleOptionToggle = (filterName: string, option: string) => {
+    switch (filterName) {
+      case "Min Price":
+        dispatch(setMinPrice(parseInt(option)));
+        break;
+      case "Max Price":
+        dispatch(setMaxPrice(parseInt(option)));
+        break;
+      case "Beds":
+        dispatch(setBeds(parseInt(option)));
+        break;
+      case "Room Types":
+        dispatch(setRoomTypes([option]));
+        break;
+      case "Bed Type":
+        dispatch(setBedType(option));
+        break;
+      default:
+        break;
+    }
+  };
 
   const filters = [
     {
-      filterName: 'Bed Type',
+      filterName: 'Min Price',
       show: true,
-      options: ['King', 'Queen'],
-      selectedOptions: [], 
+      options: ['100', '200', '300'], // Example options
     },
     {
-      filterName: 'Room Type',
+      filterName: 'Max Price',
+      show: true,
+      options: ['200', '300', '400'], // Example options
+    },
+    {
+      filterName: 'Beds',
+      show: true,
+      options: ['1', '2', '3'], // Example options
+    },
+    {
+      filterName: 'Room Types',
       show: true,
       options: ['Deluxe', 'Suite'],
-      selectedOptions: [], 
     },
     {
-      filterName: 'Price',
+      filterName: 'Bed Type',
       show: true,
-      options: ['<$100', '<$200'],
-      selectedOptions: [],
+      options: ['double', 'single'],
     },
   ];
-  
 
   return (
     <StyledBox>
       <StyledAccordion>
-        <StyledAccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-        <StyledTypography>
-          {"Narrow Your Results"}
-        </StyledTypography>                
+        <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <StyledTypography>{"Narrow Your Results"}</StyledTypography>
         </StyledAccordionSummary>
         <StyledAccordionDetails>
           <div>
             {filters.map((filter) => {
               return (
                 filter.show && (
-                  <StyledAccordion
-                    key={filter.filterName}
-                  >
+                  <StyledAccordion key={filter.filterName}>
                     <StyledAccordionSummary
                       expandIcon={<ExpandMoreIcon />}
                       aria-controls="panel1a-content"
@@ -67,11 +179,12 @@ const Filter = () => {
                     <StyledAccordionDetails>
                       {filter.options.map((option) => {
                         return (
-                          <MenuItem key={option}>
+                          <MenuItem key={option} onClick={() => handleOptionToggle(filter.filterName, option)}>
                             <ListItemIcon>
                               <Checkbox
                                 disabled={false}
-                                checked={filter.selectedOptions?.includes(option)}
+                                // checked={/* Add your checked logic here */}
+                                
                               />
                               <OptionText>{option}</OptionText>
                             </ListItemIcon>
